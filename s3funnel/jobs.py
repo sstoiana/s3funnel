4202b1c4eba051e05a54b5576df70ef2b58d744a
shazow <shazow@d299a114-3648-0410-bf92-a11f6cf122dc>
2008-06-12 22:39:43 +0000

    Refactored the code. The actual processing jobs and methods are in s3funnel/jobs.py. Added new operations: DROP, CREATE, DELETE Added start_key function to LIST (it actually does something useful now). Added retry for LIST. Overriding functionality for LIST if no bucket is given, will list buckets. Added debugging mode with double verbose. (-vv flag) Version bump to 0.5.
    git-svn-id: http://s3funnel.googlecode.com/svn/trunk@24 d299a114-3648-0410-bf92-a11f6cf122dc

---
+++ s3funnel/jobs.py
@@ -0,0 +1,174 @@
+# jobs.py - Collection of workerpool Job objects
+# Copyright (c) 2008 Andrey Petrov
+#
+# This module is part of s3funnel and is released under
+# the MIT license: http://www.opensource.org/licenses/mit-license.php
+
+from workerpool import Job
+import boto
+
+import os
+import logging
+log = logging.getLogger()
+
+# Various exceptions we're expecting
+from boto.exception import BotoServerError, BotoClientError
+from httplib import IncompleteRead
+from socket import error as SocketError
+
+# Jobs
+
+class GetJob(Job):
+    "Download the given key from S3."
+    def __init__(self, key, config={}):
+        self.key = key
+        self.retries = config.get('retry', 5)
+
+    def run(self, toolbox):
+        k = toolbox.bucket.new_key(self.key)
+        wait = 1
+        for i in xrange(self.retries):
+            try:
+                # Note: This creates a file, even if the download fails
+                k.get_contents_to_filename(self.key)
+                log.info("Got: %s" % self.key)
+                return
+            except BotoServerError, e:
+                log.error("Failed to get: %s" % self.key)
+                os.unlink(self.key) # Remove file since download failed
+                return
+            except (IncompleteRead, SocketError, BotoClientError), e:
+                log.warning("Caught exception: %r.\nRetrying..." % e)
+                wait = (2 ** wait) / 2.0 # Exponential backoff
+                time.sleep(wait)
+
+class PutJob(Job):
+    "Upload the given file to S3, where the key corresponds to basename(path)"
+    def __init__(self, path, config={}):
+        self.key = os.path.basename(path)
+        self.path = path
+        self.retries = config.get('retry', 5)
+        self.acl = config.get('acl')
+        if self.acl not in ['public-read', 'private']:
+            self.acl = 'private'
+
+    def run(self, toolbox):
+        k = toolbox.bucket.new_key(self.key)
+        wait = 1
+        headers = {'x-amz-acl': self.acl}
+
+        for i in xrange(self.retries):
+            try:
+                # Note: This creates a file, even if the download fails
+                k.set_contents_from_filename(self.path, headers)
+                log.info("Sent: %s" % self.key)
+                return
+            except BotoServerError, e:
+                log.error("Failed to put: %s" % self.key)
+                return
+            except (IncompleteRead, SocketError, BotoClientError), e:
+                log.warning("Caught exception: %r.\nRetrying..." % e)
+                wait = (2 ** wait) / 2.0 # Exponential backoff
+                time.sleep(wait)
+
+class DeleteJob(Job):
+    "Delete the given key from S3."
+    def __init__(self, key, config={}):
+        self.key = key
+        self.retries = config.get('retry', 5)
+
+    def run(self, toolbox):
+        wait = 1
+        for i in xrange(self.retries):
+            try:
+                k = toolbox.bucket.delete_key(self.key)
+                log.info("Deleted: %s" % self.key)
+                return
+            except BotoServerError, e:
+                log.error("Failed to delete: %s" % self.key)
+                return
+            except (IncompleteRead, SocketError, BotoClientError), e:
+                log.warning("Caught exception: %r.\nRetrying..." % e)
+                wait = (2 ** wait) / 2.0 # Exponential backoff
+                time.sleep(wait)
+
+
+# Helpers
+
+def _create_connection(**config):
+    "Given a configuration, return an s3 connection."
+    aws_key, aws_secret_key = config.get('aws_key'), config.get('aws_secret_key')
+    conn = boto.connect_s3(aws_key, aws_secret_key)
+    return conn
+
+# Singlethreaded methods
+# TODO: Move these to a separate module?
+# TODO: Remove the toolbox_factory parameter, use _create_connection for everything instead?
+
+def show_buckets(**config):
+    """
+    List all the buckets in the account.
+    """
+    conn = _create_connection(**config)
+    log.info("Listing buckets.")
+    r = conn.get_all_buckets()
+    for b in r:
+        print b.name
+
+def list_bucket(toolbox_factory, **config):
+    """
+    If no bucket is given, redirect call to ``show_buckets``. Otherwise:
+    List all the keys in the bucket created by ``toolbox_factory``.
+    Optionally, ``config`` may contain start_key which is used for the start
+    marker for the list operation.
+    """
+    if not config.get('bucket'):
+        return show_buckets(**config)
+    toolbox = toolbox_factory()
+    marker = config.get('start_key')
+    more_results = True
+    k = None
+    log.info("Listing keys from marker: %s" % marker)
+    while more_results:
+        try:
+            r = toolbox.bucket.get_all_keys(marker=marker)
+            for k in r:
+                print k.name
+            if k:
+                marker = k.name
+            more_results= r.is_truncated
+        except BotoServerError, e:
+            log.error("List failed.")
+            return
+        except (IncompleteRead, SocketError, BotoClientError), e:
+            log.warning("Caught exception: %r.\nRetrying..." % e)
+            wait = (2 ** wait) / 2.0 # Exponential backoff
+            time.sleep(wait)
+    log.info("Done listing bucket: %s" % config['bucket'])
+
+def create_bucket(toolbox_factory, **config):
+    """
+    Create a bucket named ``config['bucket']``.
+    """
+    name = config['bucket']
+    conn = _create_connection(**config)
+    try:
+        b = conn.create_bucket(name)
+        log.info("Created bucket: %s" % name)
+    except BotoServerError, e:
+        log.error("Bucket `%s` could not be created. Try a different name." % name)
+        return
+
+def drop_bucket(toolbox_factory, **config):
+    """
+    Delete the bucket in the ``toolbox_factory``.
+    """
+    toolbox = toolbox_factory()
+    name = toolbox.bucket.name
+    try:
+        toolbox.conn.delete_bucket(name)
+        log.info("Deleted bucket: %s" % name)
+    except BotoServerError, e:
+        log.error("Bucket `%s` could not be deleted." % name)
+        return
+
